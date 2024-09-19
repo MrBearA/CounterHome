2fb00955cd468579535c7bfc3a2830f4748cbbc8
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-19 15:54:01 +0800

    norefresh

--- src/MyNavBar.js
+++ src/MyNavBar.js
@@ -3,18 +3,19 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function MyNavbar() {
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">CIIT ACT2</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">CIIT ACT2</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/counter">Counter</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/counter">Counter</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -22,5 +23,5 @@ function MyNavbar() {
   );
 }
 
-export default MyNavbar;
+export default MyNavbar;  
 
